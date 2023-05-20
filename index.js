c07690f8bd8520e1f9ff1eb1479e7eb156dadb19
Dhruv Chandak <116879117+dhruv302003@users.noreply.github.com>
2023-05-20 17:11:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function call() {
     let country = document.getElementById("country").value;
     let text = "http://api.weatherapi.com/v1/current.json?key=e44917d5033a4b33bb3105551232005&q=" + country + "&aqi=no";
 
-
     let p = fetch("http://api.weatherapi.com/v1/current.json?key=e44917d5033a4b33bb3105551232005&q=London&aqi=no");
     p.then((value1) => {
         return value1.json();
