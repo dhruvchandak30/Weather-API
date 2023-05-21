179fa69eb4d903f2cabe3d31d53f337122da2b18
Dhruv Chandak <116879117+dhruv302003@users.noreply.github.com>
2023-05-21 15:51:40 +0530

    API

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function call() {
-    let country = document.getElementById("country").value;
-    let text = "http://api.weatherapi.com/v1/current.json?key=e44917d5033a4b33bb3105551232005&q=" + country + "&aqi=no";
+//Landing Page
+function Call(event) {
+    let country = event.target.getAttribute("value");
+    localStorage.setItem("Country_Name", country);
+    document.getElementById("Options").style.display="none";
+    const api_url = `http://api.weatherapi.com/v1/current.json?key=e44917d5033a4b33bb3105551232005&q=`+country+`&aqi=no`;
+    result(api_url);
+    
+}
 
-    let p = fetch("http://api.weatherapi.com/v1/current.json?key=e44917d5033a4b33bb3105551232005&q=London&aqi=no");
-    p.then((value1) => {
-        return value1.json();
-    }).then((value2) => {
-        console.log(value2);
-    })
+function Display() {
+    document.getElementById("Countries").style.display = "block";
+    document.getElementById("select").style.display="none";
+}
+
+// Result Page
+async function result(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    show(data);
+}
+
+
+function show(data) {
+
+    let text = "";
+    let i = 0;
+    let country = localStorage.getItem("Country_Name");
+    text += `Region : ${country} \n`;
+    i++;
+    text += "Temperature : " + data.current[Object.keys(data.current)[i]] + " \xB0C\n";
+    i++;
+    text += "Temperature : " + data.current[Object.keys(data.current)[i]] + " \xB0F\n";
+    i++;
+    i++;
+    text += "Wind Speed : " + data.current[Object.keys(data.current)[i]] + " km/hr\n";
+    i++;
+    text += "Humidity : " + data.current[Object.keys(data.current)[i]] + " %\n";
+    i++;
+    text += "Cloud : " + data.current[Object.keys(data.current)[i]] + "\n";
+    i++;
+    text += "UV : " + data.current[Object.keys(data.current)[i]] + "\n";
+    document.getElementById("Data_Box").style.display="block";
+    document.getElementById("Expectations").style.display="block";
+    document.getElementById("Data").innerText = text;
 }
