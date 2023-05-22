7419b5d34b0c172fec1b712ed4385f0c0828b154
Dhruv Chandak <116879117+dhruv302003@users.noreply.github.com>
2023-05-22 20:25:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-//Landing Page
-function Call(event) {
-    let country = event.target.getAttribute("value");
-    localStorage.setItem("Country_Name", country);
-    document.getElementById("Options").style.display="none";
-    const api_url = `https://api.weatherapi.com/v1/current.json?key=e44917d5033a4b33bb3105551232005&q=`+country+`&aqi=no`;
-    result(api_url);
-    
+// Get Value using ENTER KEY
+function Enter() {
+    var input = document.getElementById("Region");
+    input.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("select").click();
+        }
+    });
 }
 
+
+//LANDING PAGE
 function Display() {
-    document.getElementById("Countries").style.display = "block";
-    document.getElementById("select").style.display="none";
+    let region = document.getElementById("Region").value;
+    document.getElementById("select").style.display = "none";
+    document.getElementById("Region").style.display = "none";
+    localStorage.setItem("Country_Name", region);
+    const api_url = `https://api.weatherapi.com/v1/current.json?key=e44917d5033a4b33bb3105551232005&q=` + region + `&aqi=no`;
+    result(api_url);
 }
 
-// Result Page
+// RESULT PAGE
 async function result(url) {
     const response = await fetch(url);
     var data = await response.json();
     show(data);
 }
 
-
+// TO SHOW DATA
 function show(data) {
 
     let text = "";
     let i = 0;
     let country = localStorage.getItem("Country_Name");
-    text += `Region : ${country} \n`;
+    text += "Name : " + data.location[Object.keys(data.location)[i]] + "\n ";
     i++;
+    text += "Region : " + data.location[Object.keys(data.location)[i]] + "\n ";
+    i++;
+    text += "Country : " + data.location[Object.keys(data.location)[i]] + "\n ";
+    i = 1;
     text += "Temperature : " + data.current[Object.keys(data.current)[i]] + " \xB0C\n";
     i++;
     text += "Temperature : " + data.current[Object.keys(data.current)[i]] + " \xB0F\n";
@@ -40,10 +51,11 @@ function show(data) {
     text += "Cloud : " + data.current[Object.keys(data.current)[i]] + "\n";
     i++;
     text += "UV : " + data.current[Object.keys(data.current)[i]] + "\n";
-    document.getElementById("Data_Box").style.display="block";
-    document.getElementById("Expectations").style.display="block";
-    document.getElementById("Weather").style.display="block";
+    document.getElementById("Data_Box").style.display = "block";
+    document.getElementById("Expectations").style.display = "block";
+    document.getElementById("Weather").style.display = "block";
     document.getElementById("Data").innerText = text;
+    localStorage.removeItem("Country_Name");
     console.log(text);
 }
 
